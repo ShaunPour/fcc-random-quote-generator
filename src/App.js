1ca9916c8e99a28430ad06d72f085ad62501b6a3
Shaun <scpour@gmail.com>
2022-09-26 09:51:33 -0400

    Random Quote Generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [quotes, setQuotes] = useState('');
+
+  const getQuote = () => {
+    fetch('https://type.fit/api/quotes')
+    .then(res => res.json())
+    .then(data => {
+      let randomNumber = Math.floor(Math.random() * data.length);
+      setQuotes(data[randomNumber]);
+    })
+  }
+
+useEffect(() => {
+  getQuote();
+}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='quote' id="quote-box">
+        <p id="text">"{quotes.text}"</p>
+        <p id="author">{quotes.author}</p>
+        <div className='buttonContainer'>
+          <button id="new-quote" onClick={getQuote} className='btn'>Get Quote</button>
+          <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text="${quotes.text}" - ${quotes.author}`} className='btn' target='_blank' rel='noopener noreferrer'>
+            Tweet
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
